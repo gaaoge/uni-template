e0f4e17ef7418d33d7374bf185d3b88dddec30b3
gaoge <gaoge@corp.netease.com>
2020-07-28 12:53:13 +0800

    优化平台配置。

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 module.exports = (api, options) => {
-  api.extendPackage(pkg => {
+  api.extendPackage((pkg) => {
     delete pkg.postcss
     delete pkg.browserslist
     return {
@@ -7,20 +7,19 @@ module.exports = (api, options) => {
       description: options.description,
       author: options.author,
       scripts: {
-        'serve':
-          'cross-env NODE_ENV=development UNI_PLATFORM=mp-weixin vue-cli-service uni-build --mode development --watch & cross-env UNI_PLATFORM=h5 vue-cli-service uni-serve --mode development',
-        'build':
-          'cross-env NODE_ENV=production UNI_PLATFORM=mp-weixin vue-cli-service uni-build --mode production --watch',
+        'serve': `cross-env NODE_ENV=development UNI_PLATFORM=${options.platform} vue-cli-service uni-build --mode development --watch & cross-env UNI_PLATFORM=h5 vue-cli-service uni-serve --mode development`,
+        'build': `cross-env NODE_ENV=production UNI_PLATFORM=${options.platform} vue-cli-service uni-build --mode production --watch`,
         'lint': 'vue-cli-service lint',
-        'upload': 'node uploader.js'
+        'upload': 'node uploader.js',
       },
       dependencies: {
         '@dcloudio/uni-h5': '^0.7.4',
         [`@dcloudio/uni-${options.platform}`]: '^0.0.969',
         '@mf2e/ant-wechat-sdk': '0.0.3',
+        'core-js': '^3.6.5',
         'flyio': '^0.6.14',
         'vue': '^2.6.10',
-        'vuex': '^3.0.1'
+        'vuex': '^3.0.1',
       },
       devDependencies: {
         '@dcloudio/uni-cli-shared': '^0.2.983',
@@ -42,14 +41,14 @@ module.exports = (api, options) => {
         'postcss-autosize': '^1.0.1',
         'postcss-preset-env': '^6.7.0',
         'tinify-loader': '^0.2.4',
-        'vue-template-compiler': '^2.6.10'
+        'vue-template-compiler': '^2.6.10',
       },
-      browserslist: ['> 1%', 'last 2 versions', 'Android >= 4.4', 'iOS >= 8']
+      browserslist: ['> 1%', 'last 2 versions', 'Android >= 4.4', 'iOS >= 8'],
     }
   })
 
-  api.render(files => {
-    Object.keys(files).forEach(name => {
+  api.render((files) => {
+    Object.keys(files).forEach((name) => {
       delete files[name]
     })
   })
--- prompts.js
+++ prompts.js
@@ -2,33 +2,33 @@ module.exports = [
   {
     name: 'name',
     type: 'input',
-    message: '项目名称'
+    message: '项目名称',
   },
   {
     name: 'description',
     type: 'input',
-    message: '项目描述'
+    message: '项目描述',
   },
   {
     name: 'author',
     type: 'input',
-    message: '项目作者'
-  },
-  {
-    name: 'appid',
-    type: 'input',
-    message: '小程序Id'
-  },
-  {
-    name: 'projectId',
-    type: 'input',
-    message: '蚂蚁统计Id'
+    message: '项目作者',
   },
   {
     name: 'platform',
     type: 'list',
     message: '小程序平台',
     choices: ['mp-weixin', 'mp-alipay', 'mp-baidu', 'mp-toutiao'],
-    default: 'mp-weixin'
-  }
+    default: 'mp-weixin',
+  },
+  {
+    name: 'appid',
+    type: 'input',
+    message: '小程序Id',
+  },
+  {
+    name: 'projectId',
+    type: 'input',
+    message: '蚂蚁统计Id',
+  },
 ]
